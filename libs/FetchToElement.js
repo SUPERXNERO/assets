53d93704455f25da4445d03a1cd5ff3623e4355b
SUPERXNERO <110180629+SUPERXNERO@users.noreply.github.com>
2024-11-04 22:45:18 +0300

    update FetchToElement.js

--- libs/FetchToElement.js
+++ libs/FetchToElement.js
@@ -1,7 +1,7 @@
 // https://superxnero.github.io/assets/libs/another.js
 class FetchToElement() {
   constructor() {
-    
+    this.elements = reloadScanningElements();
   }
   
   reloadFetching() {
