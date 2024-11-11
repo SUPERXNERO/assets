68cf1a954290329ac48e0bfd56745f9a9883d950
SUPERXNERO <110180629+SUPERXNERO@users.noreply.github.com>
2024-11-12 00:03:38 +0300

    update LanguageManager.js

--- libs/LanguageManager.js
+++ libs/LanguageManager.js
@@ -142,7 +142,13 @@ class LanguageManager {
         textupdatemethod: textoptions[3]
       };
     } else if (typeof textoptions === "string") {
-      
+      textoptions = textoptions.split(",");
+      textoptions = {
+        textid: textoptions[0],
+        isclass: textoptions[1],
+        subtextid: textoptions[2],
+        textupdatemethod: textoptions[3]
+      }
     }
     if (textoptions.constructor !== Object) {
       console.error("Invalid textoptions format. Expected an object or array.");
